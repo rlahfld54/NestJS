d4e4a6f4e51a64c1d49cc49b5a90f37cb0ffc48d
rlahfld54 <rlahfld54@naver.com>
2022-12-30 11:19:36 +0900

    싱글톤 패턴

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,55 @@
 import * as express from "express";
 import catsRouter from "./cats/cats.route";
-const app: express.Express = express();
-const port: number = 8000;
 
-//* logging middleware
-app.use((req, res, next) => {
-  console.log(req.rawHeaders[1]);
-  console.log("This is logging middleware!");
-  next();
-});
+class Server {
+  public app: express.Application;
 
-//* json middleware  => express에서는  json형식을 바꿔주는 것이 없어서
-// json 미들웨어를 따로 만들어 줘야함
-app.use(express.json());
+  constructor() {
+    const app: express.Express = express();
+    this.app = app;
+  }
 
-// 미들웨어 라우트를 등록해준다.
-app.use(catsRouter);
+  private setRoute() {
+    // 미들웨어 라우트를 등록해준다.
+    this.app.use(catsRouter);
+  }
 
-//* 404 middleware
-app.use((req, res, next) => {
-  console.log("This is error middleware!");
-  res.send({ error: "404 not found error!" });
-});
+  private setMiddleware() {
+    //* logging middleware
+    this.app.use((req, res, next) => {
+      console.log(req.rawHeaders[1]);
+      console.log("This is logging middleware!");
+      next();
+    });
 
-// 8080 서버를 연다
-app.listen(port, () => {
-  console.log(`server is on... http://localhost:${port}`);
-});
+    //* json middleware  => express에서는  json형식을 바꿔주는 것이 없어서
+    // json 미들웨어를 따로 만들어 줘야함
+    this.app.use(express.json());
+
+    this.setRoute();
+
+    //* 404 middleware
+    this.app.use((req, res, next) => {
+      console.log("This is error middleware!");
+      res.send({ error: "404 not found error!" });
+    });
+  }
+
+  /**
+   * listen
+   */
+  public listen() {
+    this.setMiddleware();
+    // 8080 서버를 연다
+    this.app.listen(8000, () => {
+      console.log(`server is on... http://localhost:8000`);
+    });
+  }
+}
+
+function init() {
+  const server = new Server();
+  server.listen();
+}
+
+init();
